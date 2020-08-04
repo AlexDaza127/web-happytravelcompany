38741a30aad01cdccd8036a739ba2666992f1f42
Michael <Michael@LAPTOP-2DE4KAKH>
2020-08-03 20:40:11 -0500

    Se crea la carpeta para el modulo login

--- src/componentes/principal/Destinos.js
+++ src/componentes/principal/Destinos.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 
 
+//imagenes importadas
+import cab1 from '../../imagenes/cabañas1.jpg';
+import cab2 from '../../imagenes/cabañas2.jpg';
+import cab3 from '../../imagenes/cabañas3.jpg';
+import cas1 from '../../imagenes/casas1.jpg';
+import cas2 from '../../imagenes/casas2.jpg';
+
+import '../../estilos/BodyLanding.css';
+
+
 class Destinos extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>
-                <h1>Destinos</h1>
+    render() {
+        return (
+            <div className='row'>
+                <div className='row'>
+                    <div className='col-7'>
+                        <img className='' src={cab3} alt="cabaña3" width='30%' />
+                    </div>
+                    <div className='col-5'>
+                        <img className='' src={cab2} alt="cabaña3" width='30%' />
+                        <img className='' src={cab1} alt="cabaña3" width='30%' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-5 d-flex'>
+
+                        <img className='' src={cab2} alt="cabaña3" width='30%' />
+                        <img className='' src={cab3} alt="cabaña3" width='30%' />
+                    </div>
+                    <div className='col-7'>
+                        <img className='' src={cab1} alt="cabaña3" width='30%' />
+                    </div>
+                </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Destinos;
